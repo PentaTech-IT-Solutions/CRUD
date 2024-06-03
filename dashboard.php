c5a82911a701918c02705b9ee4602665efeae7cb
emma-boamah <134923735+emma-boamah@users.noreply.github.com>
2024-06-03 13:45:18 -0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,67 +1,53 @@
+<?php
+session_start();
+$dbName = 'test';
+$dbConn = new mysqli('localhost', 'root', '', 'test');
+if($dbConn->connect_error){
+    die('error connecting');
+}
+
+$query1 = "SELECT * FROM $dbName.members";
+$result1 = $dbConn->query($query1);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Members</title>
     <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
-<header>
-    <nav>
-        <button class="add-member-btn">Add Member</button>
-    </nav>
-</header>
-<main>
-    <section class="member-list">
-        <h2>Members</h2>
-        <form action="add_member_handler.php" method="post" enctype="multipart/form-data">
-            <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Image</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td><input type="text" name="name"></td>
-                    <td><input type="text" name="email"></td>
-                    <td><input type="tel" name="tel"></td>
-                    <td><input type="file" name="img_file"></td>
-                    <td>
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </td>
-                </tr>
-<!--                <tr>-->
-<!--                    <td><input type="text" name="name2"></td>-->
-<!--                    <td><input type="text" name="email2"></td>-->
-<!--                    <td><input type="text" name="tel2"></td>-->
-<!--                    <td><img src="#" alt="" name="image2"></td>-->
-<!--                    <td>-->
-<!--                        <button class="edit-btn">Edit</button>-->
-<!--                        <button class="delete-btn">Delete</button>-->
-<!--                    </td>-->
-<!--                </tr>-->
-<!--                <tr>-->
-<!--                    <td><input type="text" name="name3"></td>-->
-<!--                    <td><input type="text" name="email3"></td>-->
-<!--                    <td><input type="tel" name="tel3"></td>-->
-<!--                    <td><img src="" alt="" name="image2"></td>-->
-<!--                    <td>-->
-<!--                        <button class="edit-btn">Edit</button>-->
-<!--                        <button class="delete-btn">Delete</button>-->
-<!--                    </td>-->
-<!--                </tr>-->
-                </tbody>
-            </table>
-            <button class="add-member-btn" name="submit">Submit</button>
-        </form>
-    </section>
-</main>
+<h1>Members</h1>
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Occupation</th>
+        <th>Image</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <?php
+        while($row = $result1->fetch_assoc())
+        {
+        ?>
+        <td><?php echo $row['id'];?></td>
+        <td><?php echo $row['name'];?></td>
+        <td><?php echo $row['email'];?></td>
+        <td><?php echo $row['phone'];?></td>
+        <td><?php echo $row['occupation'];?></td>
+        <td><img src="./images/<?php echo $row['image'];?>" width="100px" alt="picture"></td>
+    </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
 </body>
 </html>
